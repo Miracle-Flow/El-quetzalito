5bdc5f2d2a18ec0d7ed1b1a185d3c0e97a27a9c7
Pratik Thapw <pratikthapw@gmail.com>
2026-07-09 21:11:19 +0545

    server function

--- app/(payload)/layout.tsx
+++ app/(payload)/layout.tsx
@@ -1,10 +1,11 @@
+import React from "react";
+
+import "@payloadcms/next/css";
 /* THIS FILE WAS GENERATED AUTOMATICALLY BY PAYLOAD. */
 /* DO NOT MODIFY IT BECAUSE IT COULD BE REWRITTEN AT ANY TIME. */
 import config from "@payload-config";
-import "@payloadcms/next/css";
-import type { ServerFunctionClient } from "payload";
 import { handleServerFunctions, RootLayout } from "@payloadcms/next/layouts";
-import React from "react";
+import type { ServerFunctionClient } from "payload";
 
 import { importMap } from "./admin/importMap.js";
 
@@ -12,7 +13,7 @@ type Args = {
   children: React.ReactNode;
 };
 
-const serverFunction: ServerFunctionClient = async function (args) {
+const serverFunction: ServerFunctionClient = async function serverFunction(args) {
   "use server";
   return handleServerFunctions({
     ...args,
